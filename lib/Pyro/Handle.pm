fe64740296b6df3d0b8c04e2709629ab89640b72
lestrrat <lestrrat+github@gmail.com>
2009-11-01 02:45:46 +0900

    subclass AnyEvent::Handle

---
+++ lib/Pyro/Handle.pm
@@ -0,0 +1,58 @@
+package Pyro::Handle;
+use Moose;
+use MooseX::NonMoose;
+use namespace::clean -except => qw(meta);
+
+extends 'AnyEvent::Handle';
+
+{
+    foreach my $handler qw(on_eof on_error on_timeout on_drain) {
+        override $handler => sub {
+            if (@_ >= 2) {
+                super();
+            } else {
+                return $_[0]->{$handler};
+            }
+        };
+    
+        my $short = $handler;
+        $short =~ s/^on_//;
+        has $handler => (
+            is => 'bare',
+            handles => {
+                "unshift_${short}_callback" => 'unshift_callback',
+                "add_${short}_callback" => 'add_callback',
+            }
+        );
+    }
+}
+
+around BUILDARGS => sub {
+    my ($next, $class, @args) = @_;
+    my $args = $next->($class, @args);
+
+    foreach my $handler qw(on_eof on_error on_timeout on_drain) {
+        $args->{$handler} ||= Pyro::Hook->new();
+    }
+
+    return $args;
+};
+
+sub BUILD {
+    my $self = shift;
+    my $finalizer = sub {
+        if ($self) {
+            close($self->fh) if $self->fh;
+            $self->destroy();
+        }
+    };
+
+    foreach my $handler qw(on_eof on_error on_timeout) {
+        $self->$handler->add_callback( $finalizer );
+    }
+    return $self;
+};
+
+__PACKAGE__->meta->make_immutable(inline_constructor => 0);
+
+1;
