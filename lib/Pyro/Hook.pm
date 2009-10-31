14166c17d598426509da859f1704787691e014bd
lestrrat <lestrrat+github@gmail.com>
2009-11-01 02:45:07 +0900

    stackable hook

---
+++ lib/Pyro/Hook.pm
@@ -0,0 +1,39 @@
+package Pyro::Hook;
+use Moose;
+use overload
+    '&{}' => sub {
+        my $self = shift;
+        return sub { $self->execute() }
+    },
+    fallback => 1,
+;
+
+has callbacks => (
+    traits => ['Array'],
+    is => 'ro',
+    isa => 'ArrayRef',
+    lazy_build => 1,
+    handles => {
+        unshift_callback => 'unshift',
+        add_callback => 'push',
+        all_callbacks => 'elements',
+        get_callback_at => 'get',
+    }
+);
+
+sub _build_callbacks { [] }
+
+sub execute {
+    my $self = shift;
+    foreach my $callback ( $self->all_callbacks ) {
+        $callback->();
+    }
+}
+
+__PACKAGE__->meta->make_immutable();
+
+# don't use namespace::clean with use overload
+no Moose;
+
+1;
+
